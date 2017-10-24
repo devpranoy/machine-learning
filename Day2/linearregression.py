c6931e399ed0b3eaa4f8a1268bec2681d196bedf
Pranoy <devpranoy@gmail.com>
2017-10-24 19:54:44 +0530

    Linear Regression

---
+++ Day2/linearregression.py
@@ -0,0 +1,22 @@
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.datasets import load_iris
+
+iris= load_iris()
+
+features = iris.data
+labels = iris.target
+
+from sklearn.cross_validation import train_test_split
+
+X_train,X_test ,Y_train,Y_test=train_test_split(features,labels,test_size=.3)
+
+#define classifier
+
+clf=LogisticRegression()
+clf.fit(X_train,Y_train)
+
+p=clf.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print("Accuracy = ",accuracy_score(Y_test,p) )
